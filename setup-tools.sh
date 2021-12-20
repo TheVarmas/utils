dc92c10034ff20542ff26662bdcd0e40a7d3bb60
xubuntu-rockgod <kiran.k.varma@outlook.com>
2021-12-20 17:40:55 -0500

    Removed unnecessary sudo

--- setup-tools.sh
+++ setup-tools.sh
@@ -12,7 +12,7 @@ sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.
 
 echo "Installing Powerlevel10k..."
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-sudo sh -c 'echo "ZSH_THEME="powerlevel10k/powerlevel10k" >> ~/.zshrc'
+ sh -c 'echo "ZSH_THEME="powerlevel10k/powerlevel10k" >> ~/.zshrc'
 
 echo "Downloading Nerd Fonts..."
 curl https://raw.githubusercontent.com/romkatv/powerlevel10k-media/master/MesloLGS%20NF%20Regular.ttf --output MesloLGSNFRegular.ttf
